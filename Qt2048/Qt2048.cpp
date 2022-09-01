ae11936b9f50189fe654821c0962abf3ec0af7be
JerPatterson <jeremie.patterson@polymtl.ca>
2022-09-01 10:18:21 -0400

    minor code style adjustments

--- Qt2048/Qt2048.cpp
+++ Qt2048/Qt2048.cpp
@@ -1,10 +1,9 @@
 #include "Qt2048.h"
 
-Qt2048::Qt2048(QWidget *parent)
-    : QMainWindow(parent)
-{
-    ui.setupUi(this);
+Qt2048::Qt2048(QWidget *parent) : QMainWindow(parent) {
+
 }
 
-Qt2048::~Qt2048()
-{}
+Qt2048::~Qt2048() {
+	
+}
--- Qt2048/main.cpp
+++ Qt2048/main.cpp
@@ -1,10 +1,12 @@
 #include "Qt2048.h"
 #include <QtWidgets/QApplication>
 
-int main(int argc, char *argv[])
-{
-    QApplication a(argc, argv);
-    Qt2048 w;
-    w.show();
-    return a.exec();
+
+int main(int argc, char *argv[]) {
+    QApplication app(argc, argv);
+
+    Qt2048 window;
+    window.show();
+
+    return app.exec();
 }
